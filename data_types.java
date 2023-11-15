0ae6ccb54d2a14a78897fae97e96bbc3c53c4e14
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2023-11-16 01:21:49 +0530

    Initalising Repo

---
+++ data_types.java
@@ -0,0 +1,39 @@
+class data_types
+
+{
+    public static void main(String a[])
+    {
+        System.out.println(3+5);
+        System.out.println(8+7);
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+        float marks = 9.123f;
+        byte by = 12;
+        System.out.println(marks);
+        System.out.println(by);
+
+
+        int num3 = 9;
+        System.out.println(num3);
+        byte bye = 127;
+        System.out.println(bye);
+        short sh = 558;
+        System.out.println(sh);
+        long l = 5854l;
+        System.out.println(l);
+
+        float f = 5.8f;
+        System.out.println(f);
+        double d = 5.8;
+        System.out.println(d);
+
+        char c = 'k';
+        System.out.println(c);
+
+        boolean b = true;
+        System.out.println(b);
+        
+    }
+}

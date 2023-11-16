e49191ce8435960b599a3e494f7702f9a7fefc3c
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2023-11-17 01:35:51 +0530

    done conv & cast

--- Conv_Cast.java
+++ Conv_Cast.java
@@ -2,15 +2,30 @@ class Conv_Cast
 {
     public static void main(String args[]) 
     {
+        // Casting
         byte b = 126;
         System.out.println(b);
+        int a = b;
+        System.out.println(a+1);
 
         // this is not working as byte can only store value till 127
         //byte c = 267;
         //System.out.println(c);
 
-        int a = b;
-        System.out.println(a);
+        // Conversion
+        int m = 256;
+        byte k = (byte) a;
+        System.out.println(k);
+
+        float f = 5.6f;
+        int t = (int) f;
+        System.out.println(t);
+
+        byte p = 10;
+        byte q = 20;
+        int r = p * q;
+
+        System.out.println(r);
 
     }
 }

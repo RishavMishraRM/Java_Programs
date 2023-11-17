8314344fa6d8404bdb78f8a75305def01a32e6c1
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2023-11-17 10:49:42 +0530

    operators added

---
+++ operators.java
@@ -0,0 +1,21 @@
+public class operators 
+{
+    public static void main(String args[]) 
+    {
+        int num1 = 7;
+        int num2 = 5; 
+        int result = num1 + num2;
+        System.out.println(result);
+
+        int num3 = 7;
+        int num4 = 5; 
+        int result1 = num3 % num4;
+        System.out.println(result1);
+
+        int num5 = 7;
+        //int num6 = 5; 
+        num5 += 2;
+        System.out.println(num5);
+
+    }   
+}

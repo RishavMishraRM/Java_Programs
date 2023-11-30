63952ae68ccc4a1c9ed55ad94c3406aacae57a5e
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2023-12-01 02:27:10 +0530

    switch_operator added

---
+++ switch_operator.java
@@ -0,0 +1,36 @@
+public class switch_operator {
+    public static void main (String a[])
+    {
+        int n = 3;
+        switch (n) {
+
+                case 1:
+                    System.out.println("Sunday");
+                    break;
+                case 2:
+                    System.out.println("Monday");
+                    break;
+                case 3:
+                    System.out.println("Tuesday");
+                    break;
+                case 4:
+                    System.out.println("Wednesday");
+                    break;
+                case 5:
+                    System.out.println("Thursday");
+                    break;
+                case 6:
+                    System.out.println("Friday");
+                    break;
+                case 7:
+                    System.out.println("Saturday");
+                    break;
+                default:
+                    System.out.println("Enter a valid number");
+        
+        }
+
+
+    }
+    
+}

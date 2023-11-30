6003a2192e8ab6a97512e75034f665595b2300e8
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2023-12-01 02:13:23 +0530

    turnery_opertor added

--- turnery_opertor.java
+++ turnery_opertor.java
@@ -3,11 +3,12 @@ public class turnery_opertor {
         int n = 4;
         int result = 0;
 
-        if(n%2==0)
-        result = 10;
+        // if(n%==0)
+        // result = 10;
+        // else
+        // result = 20;
 
-        else
-        result = 20;
+        result = n%2==0 ? 10 : 30;
         System.out.println(result);
     }
 }

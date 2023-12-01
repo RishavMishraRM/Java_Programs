64fb53dd76442f971bf48ede570bb6f994442c77
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2023-12-02 00:28:02 +0530

    loops added

---
+++ loops.java
@@ -0,0 +1,9 @@
+public class loops {
+    public static void main (String a[])
+    {
+        // repeat statement 4 times
+        // loop - while, do while, for
+        System.out.println("Hii");
+    }
+    
+}
